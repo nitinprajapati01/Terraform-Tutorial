2e14e471da3151d97e291e595a559c4f528bcd72
vijay <to.patelvijay@gmail.com>
2022-12-10 16:39:19 +0530

    aws terraform tutorial

--- terraform-aws-vpc/internet-gateway.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.vpc_demo.id}"
-
-  tags = {
-    Name = "internet-gateway-demo"
-  }
-}
--- terraform-remote-state/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.11.0"
-  backend "s3" {
-    bucket = "backup-state-terraform"
-    key    = "terraform/test"
-    region = "us-east-1"
-    dynamodb_table = "backend-test"
-  }
-}
